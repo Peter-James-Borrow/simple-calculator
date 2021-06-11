8adef24f79c4cbf0eb36515994085620a007997f
Peter Borrow <borrow.peter83@gmail.com>
2021-06-11 08:24:36 +0100

    Added files

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// const firstValue = parseInt(prompt('Please press first value'))
+// const operator = prompt('Please press operator ')
+// const secondValue = parseInt(prompt('please second value ')) 
+
+ if(operator==='+') {
+     console.log(firstValue + secondValue)
+ }    
+ else  {
+     console.log(firstValue - secondValue)
+ } 
+
+
